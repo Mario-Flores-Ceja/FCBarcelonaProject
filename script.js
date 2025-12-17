0d0fd61d9eb1f1ced2026e1f9592004c92376853
Mario-Flores-Ceja <mario_flores-ceja@student.davincischools.org>
2025-12-16 21:12:25 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// declare your functions
+function changePicture1() {
+  document.getElementById("j1").src = "images/eightBall.png";
+  document.getElementById("t5").textContent =
+    "Lionel Messi started his career in Barcelona and is currently the only player with the most Balondor's (8) while having the most G/A in football history (Over 1300 goal contributions) The last time he won the Balondor was in 2023 after an amazing performance in the World Cup";
+}
+function changePicture2() {
+  document.getElementById("s2").src = "images/FieldSextuple.png";
+  document.getElementById("t5").textContent =
+    "FC Barcelona is one of the two teams in Europe that have won a sextuple (Six trophies in one season) along With FC Bayern Munich. They won the Champions League, LaLiga Title, Copa Del Rey, Spanish Super Cup, Fifa Club World Cup, and the UEFA SuperCup in 2009";
+}
+function changePicture3() {
+  document.getElementById("l3").src = "images/2425LLCHAMPS.png";
+  document.getElementById("t5").textContent =
+    "Barcelona have won the 24/25 season in which they also a treble (three trophies) while scoring 170+ goals and reaching the UEFA Champions League Semi-Final for the first time since 2019!";
+}
+
+function changePicture4() {
+  document.getElementById("b4").src = "images/4inarow.webp";
+  document.getElementById("t5").textContent =
+    "Barcelona beat their rival Real Madrid four times in a row while Madrid had superstars including Mbappe, vinicius, Bellingham,Valverde, and Courtois";
+}
+
+function changePicture5() {
+  document.getElementById("c5").src = "images/tikitaka.png";
+  document.getElementById("t5").textContent =
+    "Tiki-Taka was a tactical strategy implemented by Johan Cruyff and Pep Guardiola which is when the team advances up the field with passes. They ended up winning some Champions League Trophies while using this strategy";
+}
+
+//https://www.w3schools.com/jsref/met_audio_play.asp
+function playSong() {}
+
+function resetAll() {}
+
+function showText() {
+  document.getElementById("gary").innerHTML = "Cascade";
+}
